d8e36ec2d88201c42792c9f7f9a0902daa9c2aa7
talmiller2 <talmiller@gmail.com>
2024-06-16 16:30:14 +0300

    add 2-params examples to test_slurmpool_local

--- tests/test_slurmpool_local.py
+++ tests/test_slurmpool_local.py
@@ -23,7 +23,7 @@ class test_slurmpool_local(unittest.TestCase):
         res = self.slurmpool.map(fun, points)
         self.assertEqual(res, res_expected)
 
-    def test_slurmpool_history(self):
+    def test_slurmpool_localmap_history(self):
         self.slurmpool = SlurmPool(self.work_dir, cluster='local-map')
         fun = lambda x: x ** 2
         points_1 = [2, 3, 4]
@@ -33,7 +33,7 @@ class test_slurmpool_local(unittest.TestCase):
         np.testing.assert_array_equal(self.slurmpool.points_history, np.array([points_1, points_2]))
         np.testing.assert_array_equal(self.slurmpool.values_history, np.array([res_1, res_2]))
 
-    def test_slurmpool_history_with_failed_points(self):
+    def test_slurmpool_localmap_history_with_failed_points(self):
         self.slurmpool = SlurmPool(self.work_dir, cluster='local-map')
         fun = lambda x: x ** 2
         fun_that_fails = lambda x: None
@@ -47,6 +47,15 @@ class test_slurmpool_local(unittest.TestCase):
         np.testing.assert_array_equal(res_2, None)
         np.testing.assert_array_equal(self.slurmpool.failed_points_history, points_2)
 
+
+    def test_slurmpool_localmap_2params(self):
+        self.slurmpool = SlurmPool(self.work_dir, cluster='local-map')
+        fun = lambda x: x[0] ** 2 + x[1] ** 2
+        points = [[2, 3], [3, 4], [4, 5]]
+        res_expected = [fun(point) for point in points]
+        res = self.slurmpool.map(fun, points)
+        self.assertEqual(res, res_expected)
+
     def test_slurmpool_local(self):
         self.slurmpool = SlurmPool(self.work_dir, cluster='local')
         fun = lambda x: x ** 2
@@ -54,6 +63,15 @@ class test_slurmpool_local(unittest.TestCase):
         res_expected = [fun(point) for point in points]
         res = self.slurmpool.map(fun, points)
         self.assertEqual(res, res_expected)
+
+    def test_slurmpool_local_2params(self):
+        self.slurmpool = SlurmPool(self.work_dir, cluster='local')
+        fun = lambda x: x[0] ** 2 + x[1] ** 2
+        points = [[2, 3], [3, 4], [4, 5]]
+        res_expected = [fun(point) for point in points]
+        res = self.slurmpool.map(fun, points)
+        self.assertEqual(res, res_expected)
+
 
 if __name__ == '__main__':
     unittest.main()
