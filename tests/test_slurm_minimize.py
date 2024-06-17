55c9e25dfbc8bb5096d594952a6f5e72a7fcdfda
talmiller2 <talmiller@gmail.com>
2024-06-17 10:25:45 +0300

    added test for 1 param opt, as well as testing the init_points mode

--- tests/test_slurm_minimize.py
+++ tests/test_slurm_minimize.py
@@ -6,6 +6,10 @@ from scipy.optimize import rosen
 from slurmcmc.optimization import slurm_minimize
 
 
+def loss_fun_1d(x):
+    return (x - 1) ** 2
+
+
 def loss_fun(x):
     return rosen(x)
 
@@ -22,6 +26,21 @@ class test_slurm_minimize(unittest.TestCase):
     def setUp(self):
         self.verbosity = 0
 
+    def test_slurm_minimize_1param(self):
+        np.random.seed(0)
+        self.num_params = 1
+        self.param_bounds = [[-5, 5] for _ in range(self.num_params)]
+        self.expected_minima_point = np.ones(self.num_params)
+        self.num_workers = 5
+        self.num_iters = 10
+
+        result = slurm_minimize(loss_fun=loss_fun_1d,
+                                param_bounds=self.param_bounds, num_workers=self.num_workers, num_iters=self.num_iters,
+                                cluster='local-map', verbosity=self.verbosity)
+
+        self.assertLessEqual(np.linalg.norm(result['x_min'] - self.expected_minima_point), 0.02)
+        self.assertLessEqual(result['loss_min'], 1e-3)
+
     def test_slurm_minimize_2params(self):
         np.random.seed(0)
         self.num_params = 2
@@ -32,8 +51,7 @@ class test_slurm_minimize(unittest.TestCase):
 
         result = slurm_minimize(loss_fun=loss_fun,
                                 param_bounds=self.param_bounds, num_workers=self.num_workers, num_iters=self.num_iters,
-                                cluster='local-map', verbosity=self.verbosity,
-                                )
+                                cluster='local-map', verbosity=self.verbosity)
 
         self.assertLessEqual(np.linalg.norm(result['x_min'] - self.expected_minima_point), 0.3)
         self.assertLessEqual(result['loss_min'], 0.05)
@@ -48,11 +66,40 @@ class test_slurm_minimize(unittest.TestCase):
 
         result = slurm_minimize(loss_fun=loss_fun, constraint_fun=constraint_fun,
                                 param_bounds=self.param_bounds, num_workers=self.num_workers, num_iters=self.num_iters,
-                                cluster='local-map', verbosity=self.verbosity,
-                                )
+                                cluster='local-map', verbosity=self.verbosity)
 
         self.assertLessEqual(np.linalg.norm(result['x_min'] - self.expected_minima_point), 0.5)
         self.assertLessEqual(result['loss_min'], 0.05)
+
+    def test_slurm_minimize_2params_with_constraint_from_init_points(self):
+        np.random.seed(0)
+        self.num_params = 2
+        self.param_bounds = [[-5, 5] for _ in range(self.num_params)]
+        self.expected_minima_point = np.ones(self.num_params)
+        self.num_workers = 10
+        self.num_iters = 30
+
+        init_points = [np.array([-1, -1]) + np.random.rand(2) for _ in range(self.num_workers)]
+        result = slurm_minimize(loss_fun=loss_fun, constraint_fun=constraint_fun, init_points=init_points,
+                                param_bounds=self.param_bounds, num_workers=self.num_workers, num_iters=self.num_iters,
+                                cluster='local-map', verbosity=self.verbosity)
+
+        self.assertLessEqual(np.linalg.norm(result['x_min'] - self.expected_minima_point), 0.5)
+        self.assertLessEqual(result['loss_min'], 0.05)
+
+    def test_slurm_minimize_2params_with_constraint_from_illegal_init_points(self):
+        np.random.seed(0)
+        self.num_params = 2
+        self.param_bounds = [[-5, 5] for _ in range(self.num_params)]
+        self.expected_minima_point = np.ones(self.num_params)
+        self.num_workers = 10
+        self.num_iters = 30
+
+        with self.assertRaises(ValueError):
+            init_points = [np.array([-4, -4]) + 0.1 * np.random.rand(2) for _ in range(self.num_workers)]
+            slurm_minimize(loss_fun=loss_fun, constraint_fun=constraint_fun, init_points=init_points,
+                           param_bounds=self.param_bounds, num_workers=self.num_workers, num_iters=self.num_iters,
+                           cluster='local-map', verbosity=self.verbosity)
 
     def test_slurm_minimize_3params(self):
         np.random.seed(0)
