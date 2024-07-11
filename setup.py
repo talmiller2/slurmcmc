eed60a376f69a6354c2bcd40380624e9f4724147
talmiller2 <talmiller@gmail.com>
2024-07-11 09:46:50 +0300

    add version number to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="slurmcmc",
+    version='1.0',
     author="Tal Miller",
     author_email="talmiller@gmail.com",
     packages=find_packages(),
