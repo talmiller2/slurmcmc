584b7105bc68c378c54cce4e7dd185b5b53bc879
talmiller2 <talmiller@gmail.com>
2024-07-28 15:16:06 +0300

    update test_slurmpool_slurm default

--- tests/test_map_slurm.py
+++ tests/test_map_slurm.py
@@ -15,8 +15,8 @@ class test_map_slurm(unittest.TestCase):
 
     def test_slurmpool_slurm(self):
         slurm_pool = SlurmPool(self.work_dir, job_name='test_slurmpool',
-                               # cluster='slurm', slurm_partition='socket',
-                               cluster='slurm', slurm_constraint='serial',
+                               cluster='slurm', slurm_partition='socket',
+                               # cluster='slurm', slurm_constraint='serial',
                                )
         fun = lambda x: x ** 2
         points = [2, 3, 4]
